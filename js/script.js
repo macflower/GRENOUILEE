4a8ab26a8ee594eb59ef957c8a820d98b2f6ab83
Andy <stonemm21074mm@gmail.com>
2021-02-01 18:08:28 +0500

    add slider

--- js/script.js
+++ js/script.js
@@ -1,5 +1,60 @@
-$(document).ready(function(){
-  $('.slider').slick({
-    dots: true,
+// $(document).ready(function(){
+//   $('.slider').slick({
+//     dots: true,
+//   });
+// });
+
+// Инициализируем Swiper
+new Swiper('.image-slider', {
+  // Стрелки
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  // Скролл
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    // Возможность перетаскивать скролл
+    draggable: true
+  },
+
+  // Курсор перетаскивания
+  grabCursor: true,
+  
+  // Количество слайдов для показа
+  slidesPerView: 4,
+
+  // Отступ между слайдами
+  spaceBetween: 30,
+
+  // Свободный режим
+  freeMode: true,
+
+  // Управление колесом мыши
+  mousewheel: {
+    // Чувствительность колеса мыши
+    sensitivity: 1,
+  },  
+
+  // Автовысота
+  // autoHeight: true,
+
+  // Бесконечный слайдер
+  // loop: true,
+
+  // Автопрокрутка
+  // autoplay: {
+  //   // Пауза между прокруткой
+  //   delay: 1000,
+  //   // Закончить на последнем слайде
+  //   stopOnLastSlide: false,
+  //   // Отключить после ручного переключения
+  //   disableOnInteraction: true
+  // },
+});
+
+$(function() {
+  $(".volume2").click(function() {
+      $(this).toggleClass("volume2-pressed");
   });
 });
